0f1f9911c9acf970867e4b8e46b24fab50196df7
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-09-10 21:48:51 -0400

    completed CSS and functionality for second and minute hand

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+
+
+
+const setDate = () => {
+
+    const now = new Date();
+    // will retrieve the seconds of the current minute in time
+    const seconds = now.getSeconds();
+    // now how do we transform our seconds into degrees?
+        // adding 90 degrees will allow to offset the original position which is at 12 on the clock (this problem was seen when we were approaching 60 seconds but were no where close to top of the clock)
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    // now that we have transformed out seconds into degress of a circle, we can update each second to transform the seconds hand by that degree
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+    
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+
+}
+
+setInterval(setDate, 1000);
